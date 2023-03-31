1d9342d4dd7e1d5c508bda0541b27f5683d51d0e
jscj <juancamargo03072002@gmail.com>
2023-03-30 23:00:47 -0500

    Service class implemented for background function, Notification added.

--- app/src/main/java/com/example/gpssmsr/LocationSr.kt
+++ app/src/main/java/com/example/gpssmsr/LocationSr.kt
@@ -1,6 +1,8 @@
 package com.example.gpssmsr
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -9,56 +11,69 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 
 
 class LocationSr: Service() {
     private val decimalFormat = DecimalFormat("#.#####")
 
-    private lateinit var locationManager: LocationManager
-    private lateinit var udp: Udpsender
-    private lateinit var runnable: Runnable
+    private lateinit var locationListener: LocationListener // define locationListener as a member variable
+
     private var mensaje: String = ""
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 
-    override fun onCreate() {
-        super.onCreate()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
-        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         val provider = locationManager.getBestProvider(Criteria(),true)
         val lastKnownLocation: Location = provider.let { locationManager.getLastKnownLocation(it!!)!! }
-        udp = Udpsender()
+        val udp = Udpsender()
         mensaje = "${decimalFormat.format(lastKnownLocation.latitude)};${decimalFormat.format(lastKnownLocation.longitude)}" +
                 ";${decimalFormat.format(lastKnownLocation.altitude)};${decimalFormat.format(lastKnownLocation.time)}"
         mensaje = mensaje.replace(',','.')
-        runnable = Runnable{
-            udp.enviarData("52.4.150.68",1001, mensaje)
-            udp.enviarData("44.212.144.254",1001,mensaje)
-            udp.enviarData("44.194.192.186",1001, mensaje)
-            udp.enviarData("84.239.15.140",1001,mensaje)
-        }
+
 
         val locationListener = LocationListener { p0 ->
             mensaje = "${decimalFormat.format(p0.latitude)};${decimalFormat.format(p0.longitude)};${decimalFormat.format(p0.altitude)};${decimalFormat.format(p0.time)}"
             mensaje = mensaje.replace(',','.')
         }
         if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            udp.enviarData("52.4.150.68",1001, mensaje)
+            udp.enviarData("44.212.144.254",1001,mensaje)
+            udp.enviarData("44.194.192.186",1001, mensaje)
+            udp.enviarData("84.239.15.140",1001,mensaje)
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0.00001f,locationListener)
         }
-        CoroutineScope(Dispatchers.IO).launch {
-            while (true) {
-                runnable.run()
-                delay(5000)
-            }
-        }
+        // create a notification channel for Android Oreo and higher
 
+            val channel = NotificationChannel(
+                "location_channel_id",
+                "Location Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            getSystemService(NotificationManager::class.java)?.createNotificationChannel(channel)
+
+        // create a notification
+        val notification = NotificationCompat.Builder(this, "location_channel_id")
+            .setContentTitle("Location Service")
+            .setContentText("Location Service is running in the background.")
+            .build()
+
+        // start the service in the foreground with the notification
+        startForeground(1, notification)
+
+        return START_STICKY
+    }
+
+    override fun onDestroy() {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        locationManager.removeUpdates(locationListener) // use the member variable here
+        stopForeground(STOP_FOREGROUND_REMOVE)
+        super.onDestroy()
     }
 }
--- app/src/main/java/com/example/gpssmsr/MainActivity.kt
+++ app/src/main/java/com/example/gpssmsr/MainActivity.kt
@@ -14,10 +14,6 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import java.text.DecimalFormat
 import java.text.SimpleDateFormat
 import java.util.*
@@ -59,6 +55,21 @@ class MainActivity : AppCompatActivity() {
         }
 
             if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+                val ip1 = findViewById<TextView>(R.id.ip1).text.toString()
+                val ip2 = findViewById<TextView>(R.id.ip2).text.toString()
+                val ip3 = findViewById<TextView>(R.id.ip3).text.toString()
+                val ip4 = findViewById<TextView>(R.id.ip4).text.toString()
+                val puerto1 = findViewById<TextView>(R.id.puerto1).text.toString().toInt()
+                val puerto2 = findViewById<TextView>(R.id.puerto2).text.toString().toInt()
+                val puerto3 = findViewById<TextView>(R.id.puerto3).text.toString().toInt()
+                val puerto4 = findViewById<TextView>(R.id.puerto4).text.toString().toInt()
+
+                udp.enviarData(ip1, puerto1, mensaje)
+                udp.enviarData(ip2, puerto2, mensaje)
+                udp.enviarData(ip3, puerto3, mensaje)
+                udp.enviarData(ip4, puerto4, mensaje)
+
                 latitud.text = "${lastKnownLocation.latitude}"
                 longitud.text = "${lastKnownLocation.longitude}"
                 altitud.text = "${lastKnownLocation.altitude}"
@@ -66,8 +77,10 @@ class MainActivity : AppCompatActivity() {
                 mensaje = "${decimalFormat.format(lastKnownLocation.latitude)};${decimalFormat.format(lastKnownLocation.longitude)}" +
                         ";${decimalFormat.format(lastKnownLocation.altitude)};${decimalFormat.format(lastKnownLocation.time)};${id}"
                 mensaje = mensaje.replace(',','.')
+
                 val serviceIntent = Intent(this, LocationSr::class.java)
                 startService(serviceIntent)
+
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000,0.00001f,locationListener)
 
             } else{
@@ -76,29 +89,6 @@ class MainActivity : AppCompatActivity() {
                 altitud.text = "(x"
                 tiempo.text = "(x"
             }
-        val runnable = Runnable{
-
-            val ip1 = findViewById<TextView>(R.id.ip1).text.toString()
-            val ip2 = findViewById<TextView>(R.id.ip2).text.toString()
-            val ip3 = findViewById<TextView>(R.id.ip3).text.toString()
-            val ip4 = findViewById<TextView>(R.id.ip4).text.toString()
-            val puerto1 = findViewById<TextView>(R.id.puerto1).text.toString().toInt()
-            val puerto2 = findViewById<TextView>(R.id.puerto2).text.toString().toInt()
-            val puerto3 = findViewById<TextView>(R.id.puerto3).text.toString().toInt()
-            val puerto4 = findViewById<TextView>(R.id.puerto4).text.toString().toInt()
-
-            udp.enviarData(ip1, puerto1, mensaje)
-            udp.enviarData(ip2, puerto2, mensaje)
-            udp.enviarData(ip3, puerto3, mensaje)
-            udp.enviarData(ip4, puerto4, mensaje)
-        }
-        // use UDP communication
-        CoroutineScope(Dispatchers.IO).launch {
-            while (true) {
-                runnable.run()
-                delay(5000)
-            }
-        }
         }
 
 
